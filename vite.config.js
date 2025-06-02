0c496f9d8a5968601e82f8ce4e52361aad771d45
BiruHake <biruhake26@gmail.com>
2025-06-02 17:37:06 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Login-and-SignUp-Page/', 
+ base: '/Login-and-SignUp-Page/',
   plugins: [react(),  tailwindcss()],
 })
