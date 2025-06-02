7381509cc64365fb4b912866a7c6ccd11e4c480c
BiruHake <biruhake26@gmail.com>
2025-06-02 17:50:26 +0530

    Initial commit

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -33,7 +33,7 @@ function SignUp() {
   };
 
   return (
-    <div className='bg-amber-200 w-[500px] p-4 flex flex-col justify-center items-center rounded-md shadow-4xl-white '>
+    <div className='bg-amber-200 max-w-[500px] p-4 flex flex-col justify-center items-center rounded-md shadow-4xl-white '>
       <h2 className='font-bold text-4xl bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 bg-clip-text text-transparent'>Sign Up</h2>
       <form onSubmit={handleSubmit} className='flex flex-col gap-1 mt-4 text-center'>
         <div className='mb-3'>
